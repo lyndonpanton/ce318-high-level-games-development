93b9ad4b76c3e0e638d6522ea8696219a32258f7
Lyndon Panton <LyndonPanton@gmail.com>
2023-10-18 10:26:03 +0100

    Changed ui game object types from textmeshprougui to text in order to conform with the lab

--- week_3/lab/CE318Lab1/Assets/Scripts/PlayerController.cs
+++ week_3/lab/CE318Lab1/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using System.Collections;
 using TMPro;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,8 +10,10 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     private int count;
     private int numberOfPickups = 8;
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI winText;
+    // public TextMeshProUGUI scoreText;
+    // public TextMeshProUGUI winText;
+    public Text scoreText;
+    public Text winText;
 
     private void Start()
     {
